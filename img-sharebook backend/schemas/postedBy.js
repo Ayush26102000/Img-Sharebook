21cba9c951540c01a522e0c9f5c0a82802df676b
Ayush Kamble <aykkamble098@gmail.com>
2022-12-20 12:41:37 +0530

    Backend of image share first commit

---
+++ img-sharebook backend/schemas/postedBy.js
@@ -0,0 +1,7 @@
+export default {
+    name: 'postedby',
+    title: 'Postedby',
+    type: 'reference',
+    to: [{ type: 'user' }]   
+
+  }
---
+++ img-sharebook backend/schemas/save.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',   
+    fields: [
+      {
+        name: 'postedby',
+        title: 'Postedby',
+        type: 'postedby'
+        
+      },
+      {
+        name: 'userId',
+        title: 'UserID',
+        type: 'string'
+        
+      },
+    ]
+}
