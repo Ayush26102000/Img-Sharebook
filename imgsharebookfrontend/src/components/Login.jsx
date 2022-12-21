85f000cdeed8c4e4b6b91eefceec6014046b8541
Ayush Kamble <aykkamble098@gmail.com>
2022-12-21 22:33:13 +0530

    Google authentication done

--- imgsharebookfrontend/src/components/Login.jsx
+++ imgsharebookfrontend/src/components/Login.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
+import { BsFillCameraReelsFill } from 'react-icons/bs';
 import ShareVideo from '../assets/assets/share.mp4';
-import logo from '../assets/assets/logo.png';
+
 
 const Login = () => {
+
+  const responseGoogle =(Response) => {
+
+  }
+
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='relative w-full h-full'><video src={ShareVideo}
@@ -13,18 +19,22 @@ const Login = () => {
       </video>
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 '>
           <div className='p-5'>
-           <h1>Image ShareBook</h1>
-            <div className='shadow-2xl'>
+
+            <h1 className='text-white m-6 flex' ><BsFillCameraReelsFill className='mr-4' />Image ShareBook</h1>
+            <div className='shadow-2x1'>
               <GoogleLogin
-                clientId=''
+                clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
                 render={(renderProps) => (
                   <button type='button'
                     className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none hover:opacity-80 transition duration-500 hover:scale-110'
-                  >
+                    onClick={(renderProps.onClick)}
+                    disabled={(renderProps.disabled)}>
                     <FcGoogle className='mr-4 ' /> Sign In with Google
                   </button>
                 )}
-
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy="single_host_origin"
               />
             </div>
           </div>
